de937b084f88e9bcaf026b334f7b4068dd494ec0
Thiago Freitas <tfreitasdevv@gmail.com>
2020-03-09 19:53:13 -0300

    Criada estrutura modular padrao com slidy

---
+++ lib/app/app_module.dart
@@ -0,0 +1,22 @@
+import 'package:paroquia_sao_lourenco/app/app_controller.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter/material.dart';
+import 'package:paroquia_sao_lourenco/app/app_widget.dart';
+import 'package:paroquia_sao_lourenco/app/modules/home/home_module.dart';
+
+class AppModule extends MainModule {
+  @override
+  List<Bind> get binds => [
+        Bind((i) => AppController()),
+      ];
+
+  @override
+  List<Router> get routers => [
+        Router(Modular.initialRoute, module: HomeModule()),
+      ];
+
+  @override
+  Widget get bootstrap => AppWidget();
+
+  static Inject get to => Inject<AppModule>.of();
+}
