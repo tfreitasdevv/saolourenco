5029f78826e808315b562cc1baf25ee294366ad1
Thiago Freitas <tfreitasdevv@gmail.com>
2020-07-10 19:38:30 -0300

    INC - Adicionado plugin OneSignal para Push Notifications, sem muitas configurações

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:paroquia_sao_lourenco/app/app_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -9,6 +10,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    OneSignal.shared.init('5ee0cfb4-754a-4294-ae73-f5d8ea7f4efd');
+    OneSignal.shared.setInFocusDisplayType(OSNotificationDisplayType.none);
+    OneSignal.shared.setNotificationReceivedHandler((notification) { });
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: ModularApp(module: AppModule()),
